55d0f21beeeaea6ba22a3ae8fa08ed901de983b7
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2023-08-31 15:45:31 -0700

    Finally Completed

--- hos08/backend/server.js
+++ hos08/backend/server.js
@@ -3,8 +3,16 @@ const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5050;
 const app = express();
 const connectDB = require("./config/db");
+const { errorHandler } = require("./middleware/errorHandler");
 
 connectDB();
+
+//this allows us to use the body of the request
+//you will get undefined if you don't have this
+app.use(express.json());
+//this allows us to use url encoded data
+//this is for when you submit a form
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
@@ -15,3 +23,5 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/records", require("./routes/recordRoutes"));
+
+app.use(errorHandler);
